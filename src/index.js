7cbe3fc9ceb6e4c313a8d2a84e7349d5ba9c38dd
kumarananda <coderanandakumar@gmail.com>
2022-03-31 01:40:28 +0600

    re import

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'boxicons/css/boxicons.min.css';
+import './main.scss';
+import './index.css';
+import axios from 'axios';
+
+// import achem from  './Achem.png'
+// import ananda from  './img/ANANDA.png'
+// import Student from './components/Student';
+// import Teacher from './components/Teacher';
+
